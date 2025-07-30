336bbe06626a1155a6e0228b260f6b4a8e6275f7
tharunkumar-BIT <149181225+tharunkumar-BIT@users.noreply.github.com>
2025-07-30 22:24:32 +0530

    navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import EmailVerify from "./pages/EmailVerify";
+import ResetPassword from "./pages/ResetPassword";
 
 const App = () => {
   return (
-    <div >App</div>
-  )
-}
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/email-verify" element={<EmailVerify />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
