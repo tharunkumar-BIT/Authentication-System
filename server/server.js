409d8f7c8049f0925b84bb0ef122309424b3fcbd
tharunkumar-BIT <149181225+tharunkumar-BIT@users.noreply.github.com>
2025-07-30 20:19:45 +0530

    get user details

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 
 import connectdb from "./db/db.js";
 import authRouter from "./routes/authRoutes.js";
+import userRoute from "./routes/userRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -16,5 +17,6 @@ app.use(cors({ credentials: true }));
 
 app.get("/", (req, res) => res.send("API Working"));
 app.use("/api/auth", authRouter);
+app.use("/api/user", userRoute);
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
