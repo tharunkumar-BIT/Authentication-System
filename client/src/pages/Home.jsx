bf40936e90dacc225d4e83ea6e79947223beed63
tharunkumar-BIT <149181225+tharunkumar-BIT@users.noreply.github.com>
2025-07-31 09:51:08 +0530

    header

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import React from "react";
+import Navbar from "../components/Navbar";
+import Header from "../components/Header";
 
 const Home = () => {
   return (
     <div>
-        <Navbar/>
+      <Navbar />
+      <Header />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
