8e4536f3003dac81720019ed56852e23278668af
tharunkumar-BIT <149181225+tharunkumar-BIT@users.noreply.github.com>
2025-07-31 20:44:05 +0530

    router change

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -19,7 +19,7 @@ authRouter.post("/logout", logOutUser);
 
 authRouter.post("/send-verify-otp", userAuth, sendVerifyOtp);
 authRouter.post("/verify-account", userAuth, verifyEmail);
-authRouter.post("/is-auth", userAuth, isAuthenticated);
+authRouter.get("/is-auth", userAuth, isAuthenticated);
 authRouter.post("/send-reset-otp", userAuth, sendResetOtp);
 authRouter.post("/reset-password", userAuth, resetPassword);
 
