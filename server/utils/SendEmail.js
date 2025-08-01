584afa15f1ce8b8ae2fb13dbed789fdc8180fa71
tharunkumar-BIT <149181225+tharunkumar-BIT@users.noreply.github.com>
2025-08-01 07:27:43 +0530

    email templates

--- server/utils/SendEmail.js
+++ server/utils/SendEmail.js
@@ -1,4 +1,8 @@
 import transporter from "../config/nodeMailer.js";
+import {
+  EMAIL_VERIFY_TEMPLATE,
+  PASSWORD_RESET_TEMPLATE,
+} from "../config/EmailTemplate.js";
 
 export const sendWelcomeEmail = async (email) => {
   const mailoptions = {
@@ -16,7 +20,11 @@ export const sendVerficationOtp = async (email, otp) => {
     from: process.env.SENDER_EMAIL,
     to: email,
     subject: "Account Verfication OTP",
-    text: `Your OTP is ${otp}. Verify your account using this OTP.`,
+    // text: `Your OTP is ${otp}. Verify your account using this OTP.`,
+    html: EMAIL_VERIFY_TEMPLATE.replace("{{otp}}", otp).replace(
+      "{{email}}",
+      email
+    ),
   };
 
   await transporter.sendMail(mailOptions);
@@ -27,7 +35,11 @@ export const sendPasswordResetOtp = async (email, otp) => {
     from: process.env.SENDER_EMAIL,
     to: email,
     subject: "Password Reset OTP",
-    text: `Your OTP is ${otp}. Reset your password using this OTP.`,
+    // text: `Your OTP is ${otp}. Reset your password using this OTP.`,
+    html: PASSWORD_RESET_TEMPLATE.replace("{{otp}}", otp).replace(
+      "{{email}}",
+      email
+    ),
   };
 
   await transporter.sendMail(mailOptions);
